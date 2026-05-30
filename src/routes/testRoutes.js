87968850224c196847bbfe445aab37fd19bc61d7
Luiz Carlos Borges D'Amico <luizcbdamico1@gmail.com>
2026-05-29 21:40:29 -0300

    .

--- src/routes/testRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-
-const testController = require("../controllers/testController")
-
-router.get("/", testController.testConection)
-router.get("/tables", testController.listTables)
-
-module.exports = router
--- src/sever.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app")
-
-const SEVER_PORT = 3000
-
-app.listen(SEVER_PORT, () => {
-    console.log(`API em http://localhost:${SEVER_PORT}`)
-})
